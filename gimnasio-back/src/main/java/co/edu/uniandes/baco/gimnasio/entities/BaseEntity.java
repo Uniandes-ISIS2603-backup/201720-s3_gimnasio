61ac4cabcd7055e3c932b27c5a9ad17c1d7197d0
jc.bojaca <jc.bojaca@ISIS2603S3-0023.sis.virtual.uniandes.edu.co>
2017-09-06 10:29:18 -0500

    asdsdfa

--- gimnasio-back/src/main/java/co/edu/uniandes/baco/gimnasio/entities/BaseEntity.java
+++ gimnasio-back/src/main/java/co/edu/uniandes/baco/gimnasio/entities/BaseEntity.java
@@ -23,15 +23,49 @@ SOFTWARE.
 */
 package co.edu.uniandes.baco.gimnasio.entities;
 
-import co.edu.uniandes.baco.gimnasio.entities.BaseEntity;
 import java.io.Serializable;
-import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 
 /**
+ * Generic entity with ID and name fields to inherit from.
+ *
+ * This entity sets a standar of fields and functions all entities in a project
+ * should have. For example, all entities should be compared by ID when not
+ * null, otherwise use the object equals method.
  *
  * @author ISIS2603
  */
-@Entity
-public class GimnasioEntity extends BaseEntity implements Serializable {
-   
+@MappedSuperclass
+public abstract class BaseEntity implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this.getId() != null && ((BaseEntity) obj).getId() != null) {
+            return this.getId().equals(((BaseEntity) obj).getId());
+        }
+        return super.equals(obj);
+    }
+
+    @Override
+    public int hashCode() {
+        if (this.getId() != null) {
+            return this.getId().hashCode();
+        }
+        return super.hashCode();
+    }
 }
---
+++ gimnasio-back/src/main/java/co/edu/uniandes/baco/gimnasio/persistence/ObjetivoPersistence.java
@@ -0,0 +1,85 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.baco.gimnasio.persistence;
+
+import co.edu.uniandes.baco.gimnasio.entities.ObjetivoEntity;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
+/**
+ *
+ * @author jc.bojaca
+ */
+@Stateless
+public class ObjetivoPersistence {
+    private static final Logger LOGGER = Logger.getLogger(ObjetivoPersistence.class.getName());
+    @PersistenceContext(unitName = "objetivoPU")
+    protected EntityManager em;
+
+    /**
+     * Agrega un objetivo
+     * 
+     * @param entity objeto Objetivo que se creará en la base de datos
+     * @return devuelve la entidad creada con un id dado por la base de datos.
+     */
+    public ObjetivoEntity create(ObjetivoEntity entity) {
+        LOGGER.info("Creando un objetivo nueva");
+        em.persist(entity);
+        return entity;
+    }
+
+    /**
+     * Actualiza un objetivo.
+     *
+     * @param entity: la Objetivo que viene con los nuevos cambios. Por ejemplo
+     * el codigo pudo cambiar. En ese caso, se haria uso del método update.
+     * @return un objetivo con los cambios aplicados.
+     */
+    public ObjetivoEntity update(ObjetivoEntity entity) {
+        LOGGER.log(Level.INFO, "Actualizando Objetivo con id={0}", entity.getId());
+        return em.merge(entity);
+    }
+
+    /**
+     *
+     * Borra un objetivo de la base de datos recibiendo como argumento el id
+     * de el Objetivo
+     *
+     * @param id: id correspondiente a la Objetivo a borrar.
+     */
+    public void delete(Long id) {
+        LOGGER.log(Level.INFO, "Borrando Objetivo con id={0}", id);
+        ObjetivoEntity entity = em.find(ObjetivoEntity.class, id);
+        em.remove(entity);
+    }
+
+    /**
+     * Busca si hay algun objetivo con el id que se envía de argumento
+     *
+     * @param id: id correspondiente a la Objetivo buscada.
+     * @return un objetivo.
+     */
+    public ObjetivoEntity find(Long id) {
+        LOGGER.log(Level.INFO, "Consultando Objetivo con id={0}", id);
+        return em.find(ObjetivoEntity.class, id);
+    }
+
+    /**
+     * Devuelve todas las Objetivos de la base de datos.
+     *
+     * @return una lista con todas las Objetivoes que encuentre en la base de datos
+     */
+    public List<ObjetivoEntity> findAll() {
+        LOGGER.info("Consultando todas las Objetivoes");
+        TypedQuery query = em.createQuery("select u from ObjetivoEntity u", ObjetivoEntity.class);
+        return query.getResultList();
+    }
+}
