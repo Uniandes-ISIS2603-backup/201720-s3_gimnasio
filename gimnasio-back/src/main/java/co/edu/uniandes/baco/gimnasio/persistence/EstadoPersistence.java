6c212801c4c38274123bf301bbb440158653f93a
jc.bojaca <jc.bojaca@ISIS2603S3-0023.sis.virtual.uniandes.edu.co>
2017-09-10 10:45:12 -0500

    acualizacionPersistence

--- gimnasio-back/src/main/java/co/edu/uniandes/baco/gimnasio/persistence/EstadoPersistence.java
+++ gimnasio-back/src/main/java/co/edu/uniandes/baco/gimnasio/persistence/EstadoPersistence.java
@@ -20,7 +20,7 @@ public class EstadoPersistence {
     
     private static final Logger LOGGER = Logger.getLogger(EstadoPersistence.class.getName());
     
-   @PersistenceContext(unitName = "EstadoPU")
+   @PersistenceContext(unitName = "estadoPU")
    protected EntityManager em;
    
    // metodo para crear un nuevo estado
--- gimnasio-back/src/main/java/co/edu/uniandes/baco/gimnasio/persistence/MedidaPersistence.java
+++ gimnasio-back/src/main/java/co/edu/uniandes/baco/gimnasio/persistence/MedidaPersistence.java
@@ -19,7 +19,7 @@ public class MedidaPersistence {
     
      private static final Logger LOGGER = Logger.getLogger(MedidaPersistence.class.getName());
      
-     @PersistenceContext(unitName = "MedidaPU")
+     @PersistenceContext(unitName = "medidaPU")
      protected EntityManager em;
      
      
