a36b2231cfe3a58008ebc4805478167d619b3013
CarlosEduardoRobles <31011767+CarlosEduardoRobles@users.noreply.github.com>
2017-09-11 14:28:05 -0500

    Add files via upload

---
+++ gimnasio-back/src/main/java/co/edu/uniandes/baco/gimnasio/entities/MedicionEntity.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.baco.gimnasio.entities;
+
+import java.io.Serializable;
+
+/**
+ * @author ce.robles
+ **/
+public class MedicionEntity extends BaseEntity implements Serializable
+{
+    
+    private Double medida;
+
+    public Double getMedida() {
+        return medida;
+    }
+
+    public void setMedida(Double medida) {
+        this.medida = medida;
+    }
+    
+}
---
+++ gimnasio-back/src/main/java/co/edu/uniandes/baco/gimnasio/entities/TipoMedidaEntity.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.baco.gimnasio.entities;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+
+/**
+ * @author ce.robles
+ **/
+@Entity
+public class TipoMedidaEntity extends BaseEntity implements Serializable
+{
+    
+    private String medida;
+
+    public String getMedida() {
+        return medida;
+    }
+
+    public void setMedida(String medida) {
+        this.medida = medida;
+    }
+    
+}
