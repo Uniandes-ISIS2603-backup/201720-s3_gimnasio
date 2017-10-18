5d1417f0eb513170954537f156f544be77e12991
jc.bojaca <jc.bojaca@ISIS2603S3-0023.sis.virtual.uniandes.edu.co>
2017-10-18 11:36:52 -0500

    xcv

--- gimnasio-backend/src/main/java/co/edu/uniandes/baco/gimnasio/ejb/MedidaLogic.java
+++ gimnasio-backend/src/main/java/co/edu/uniandes/baco/gimnasio/ejb/MedidaLogic.java
@@ -6,7 +6,6 @@ import co.edu.uniandes.baco.gimnasio.entities.TipoMedidaEntity;
 import co.edu.uniandes.baco.gimnasio.exceptions.BusinessLogicException;
 import co.edu.uniandes.baco.gimnasio.exceptions.NoExisteException;
 import co.edu.uniandes.baco.gimnasio.persistence.BasePersistence;
-import java.util.List;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 
@@ -16,24 +15,15 @@ import javax.inject.Inject;
  */
 
 @Stateless
-public class MedidaLogic extends BaseLogic<MedidaEntity> {
-    /**
-     * inject de la logica de estado
-     */
-    private EstadoLogic estadoLogic;
-    /**
-    * inject de la logica de medida
-    */
- 
+public class MedidaLogic extends SubResource<EstadoEntity,MedidaEntity> {
     private TipoMedidaLogic tipoMedidaLogic;
 
     public MedidaLogic() {
         super();
     }
 
-       @Inject public MedidaLogic(EstadoLogic estadoLogic, TipoMedidaLogic tipoMedidaLogic, BasePersistence<MedidaEntity> persistence) {
-        super(persistence);
-        this.estadoLogic = estadoLogic;
+       @Inject public MedidaLogic(EstadoLogic logic, TipoMedidaLogic tipoMedidaLogic, BasePersistence<MedidaEntity> persistence) {
+        super(persistence, logic, EstadoEntity::getMedidas, MedidaEntity::setEstado);
         this.tipoMedidaLogic = tipoMedidaLogic;
     }
     
@@ -46,41 +36,14 @@ public class MedidaLogic extends BaseLogic<MedidaEntity> {
      * @throws BusinessLogicException si estado no existe 
      */
     public MedidaEntity create(Long idEstado, MedidaEntity entity,Long idTipoMedida) throws BusinessLogicException {
-        EstadoEntity estado = estadoLogic.find(idEstado);
         TipoMedidaEntity parte = tipoMedidaLogic.find(idTipoMedida);
         if(parte.isAutomatico())
             throw new BusinessLogicException("no se puede asociar una medidcion manual a una medida automatica");
-        entity.setEstado(estado);
-        MedidaEntity est=create(entity);
+        MedidaEntity est=create(idEstado,entity);
         entity.setParte(parte);
         return est;
     }
-    /**
-     * metodo que encuentra todas las medidas de un etado
-     * @param idEstado
-     * @return lista de medidas del estado
-     * @throws BusinessLogicException si el estado no existe
-     */
-    public List<MedidaEntity> findAll(Long idEstado) throws BusinessLogicException {
-        return estadoLogic.find(idEstado).getMedidas();
-    }
-    /**
-     * metodo que encuentra una medida en especifico
-     * @param idEstado id del estado
-     * @param id de la medida
-     * @return la medida
-     * @throws BusinessLogicException si ele stado o la medida no existen 
-     */
-    public MedidaEntity find(Long idEstado, Long id) throws BusinessLogicException {
-        MedidaEntity ent = new MedidaEntity();
-        ent.setId(id);
-        List<MedidaEntity> list = estadoLogic.find(idEstado).getMedidas();
-        int ind = list.indexOf(ent);
-        if (ind < 0) {
-            throw new NoExisteException(id);
-        }
-        return list.get(ind);
-    }
+    
     /**
      * metodo para actulizar una medida
      * @param idEstado id del estado
@@ -95,16 +58,5 @@ public class MedidaLogic extends BaseLogic<MedidaEntity> {
         entity.setEstado(old.getEstado());
         entity.setParte(old.getParte());
         return persistence.update(entity);
-    }
-    /**
-     * metodo que elimina una medida de un estado
-     * @param idEstado id del estado
-     * @param id de la medida
-     * @throws BusinessLogicException si el estado o la medida no existe 
-     */
-     public void remove(long idEstado,long id) throws BusinessLogicException {
-        MedidaEntity ent=find(idEstado,id);
-        estadoLogic.find(idEstado).getMedidas().remove(ent);
-        remove(id);
     }
 }
