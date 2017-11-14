349e0ab39dc98fd4b9622f365be936c1992b33ae
JuanCamiloBojaca <30963726+JuanCamiloBojaca@users.noreply.github.com>
2017-11-14 16:52:30 -0500

    correcion

--- gimnasio-backend/src/main/java/co/edu/uniandes/baco/gimnasio/persistence/EjercicioHechoPersistence.java
+++ gimnasio-backend/src/main/java/co/edu/uniandes/baco/gimnasio/persistence/EjercicioHechoPersistence.java
@@ -1,10 +1,7 @@
 package co.edu.uniandes.baco.gimnasio.persistence;
 
 import co.edu.uniandes.baco.gimnasio.entities.EjercicioHechoEntity;
-import java.util.Date;
-import java.util.List;
 import javax.ejb.Stateless;
-import javax.persistence.TypedQuery;
 
 /**
  *
