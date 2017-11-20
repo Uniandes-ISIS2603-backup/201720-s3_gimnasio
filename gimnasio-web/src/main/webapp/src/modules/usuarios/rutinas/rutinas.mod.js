c72472143db64d4aba1781dfbd4ed82a8c3e5a5a
JuanPabloRomero <31632055+JuanPabloRomero@users.noreply.github.com>
2017-11-20 14:00:57 -0500

    rutinas mod

---
+++ gimnasio-web/src/main/webapp/src/modules/usuarios/rutinas/rutinas.mod.js
@@ -0,0 +1,56 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+(function (ng) {
+    // Definición:
+    var mod = ng.module("rutinaModule", ['ui.router']);
+    //constantes:
+    mod.constant("rutinaContext", "api/rutinas");
+    // Configuración:
+    mod.config(['$stateProvider', '$urlRouterProvider',
+        
+        function ($stateProvider, $urlRouterProvider) {
+            var basePath = 'src/modules/rutinas/';
+            
+            // default
+            $urlRouterProvider.otherwise("/rutinasList");
+            
+            // estados
+            $stateProvider.state('rutinas', {
+                url: '/rutinas',
+                abstract: true,
+                views: {
+                    'mainView': {
+                        templateUrl: basePath + 'rutinas.html',
+                        controller: 'rutinasCtrl',
+                        controllerAs: 'ctrl'
+                    }
+                }
+            }).state('createRutina',{
+                url:'/crear',
+                parent:'rutinas',
+                views: {
+                    'listView':{
+                        templateUrl: basePath + 'rutina.create.html',
+                        controller: 'rutinasCtrl',
+                        controllerAs: 'ctrl'
+                    }
+                }
+            }).state('rutinasList', {
+                url: '/list',
+                parent: 'rutinas',
+                views: {
+                    'listView': {
+                        templateUrl: basePath + 'rutinas.list.html',
+                        controller: 'rutinasCtrl',
+                        controllerAs: 'ctrl'
+                    }
+                }
+            });
+        }
+    ]);
+})(window.angular);
+
+
